86d8df1d1afa17d2440e8d196773abed378647c0
boksul <boksul@hotmail.com>
2019-03-12 20:16:46 +0000

    unique shortURL success

--- express_server.js
+++ express_server.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+let unique = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    unique += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return unique;
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,7 +33,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars)
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
+
+
+
+
+
+
+
+
+
