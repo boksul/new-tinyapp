bcf08ea0a3045908ebab60d6e9f4b1437a99269d
boksul <boksul@hotmail.com>
2019-03-13 17:05:36 +0000

    delete added

--- express_server.js
+++ express_server.js
@@ -55,7 +55,10 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 })
 
-
+app.post("/urls/:id/delete", (req,res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls")
+})
 
 
 
